681541b887361991a3f324845083a6859a8ac60d
OmicaKale <omicakale1305@gmail.com>
2024-09-20 12:53:55 +0530

    font size update

--- app/components/home/banner.jsx
+++ app/components/home/banner.jsx
@@ -13,8 +13,8 @@ export const Banner = () => {
                         </div>
                         <div>
                             <div className='flex justify-start lg:block'>
-                                <h1 className="text-black dark:text-white text-3xl lg:text-6xl font-semibold pt-2 whitespace-nowrap">Web</h1>
-                                <h1 className="text-black dark:text-white text-3xl lg:text-6xl font-semibold pt-2 ml-4 lg:ml-60 whitespace-nowrap">Developer</h1>
+                                <h1 className="text-black dark:text-white text-3xl lg:text-9xl font-semibold pt-2 whitespace-nowrap">Web</h1>
+                                <h1 className="text-black dark:text-white text-3xl lg:text-9xl font-semibold pt-2 ml-4 lg:ml-60 whitespace-nowrap">Developer</h1>
                             </div>
                             <p className="text-gray600 dark:text-white text-sm font-medium mt-7 w-[150%]">I'm a passionate and driven individual who recently graduated with a Bachelor's degree in Computer Engineering from Smt. Kashibai Navale College Of Engineering, Pune. My journey into the world of technology began with a keen interest in python & java development, and since then, I've been on an exhilarating path of growth and learning.
                                 
