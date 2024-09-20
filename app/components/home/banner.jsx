d2f3391d63c6546a917deaa91068a7b4e32f452c
OmicaKale <omicakale1305@gmail.com>
2024-09-20 13:02:16 +0530

    font size update

--- app/components/home/banner.jsx
+++ app/components/home/banner.jsx
@@ -13,10 +13,10 @@ export const Banner = () => {
                         </div>
                         <div>
                             <div className='flex justify-start lg:block'>
-                                <h1 className="text-black dark:text-white text-3xl lg:text-7xl font-semibold pt-2 whitespace-nowrap">Web</h1>
+                                <h1 className="text-black dark:text-white text-3xl lg:text-7xl font-semibold pt-2 whitespace-nowrap">Web -</h1>
                                 <h1 className="text-black dark:text-white text-3xl lg:text-7xl font-semibold pt-2 ml-4 lg:ml-60 whitespace-nowrap">Developer</h1>
                             </div>
-                            <p className="text-gray600 dark:text-white text-sm font-medium mt-7 w-[150%]">I'm a passionate and driven individual who recently graduated with a Bachelor's degree in Computer Engineering from Smt. Kashibai Navale College Of Engineering, Pune. My journey into the world of technology began with a keen interest in python & java development, and since then, I've been on an exhilarating path of growth and learning.
+                            <p className="text-gray600 dark:text-white text-base font-medium mt-7 w-[150%]">I'm a passionate and driven individual who recently graduated with a Bachelor's degree in Computer Engineering from Smt. Kashibai Navale College Of Engineering, Pune. My journey into the world of technology began with a keen interest in python & java development, and since then, I've been on an exhilarating path of growth and learning.
                                 
                             </p>
                             <button className='bg-primary rounded-md px-6 py-2 text-white text-sm mt-3'>
